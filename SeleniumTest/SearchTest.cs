5dbe7598a38283290f877b9cfc6163db770ce2fa
Naveen0417 <naveenkumarreddy106@gmail.com>
2021-06-17 22:28:52 +0530

    Initial commit of all files to the repository

---
+++ SeleniumTest/SearchTest.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
+
+namespace SeleniumTest
+{
+    [TestClass]
+    public class SearchTest
+    {
+        [TestMethod]
+        public void SearchTestMethod()
+        {
+            using (var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))
+            {
+
+                driver.Navigate().GoToUrl("https://www.amazon.in");
+                driver.Quit();
+            }
+        }
+    }
+}
